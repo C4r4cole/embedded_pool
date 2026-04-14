f0a84d08cc7608143390e1f9c1f75eb4cf8651c7
C4r4cole <c4r4cole.chevalerie@gmail.com>
2026-04-14 19:54:07 +0200

    ex03 done

--- ex03/main.c
+++ ex03/main.c
@@ -6,22 +6,27 @@
 /*   By: fmoulin <fmoulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/14 13:36:10 by fmoulin           #+#    #+#             */
-/*   Updated: 2026/04/14 19:40:21 by fmoulin          ###   ########.fr       */
+/*   Updated: 2026/04/14 19:52:49 by fmoulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <avr/io.h>
+#include <util/delay.h>
 
 int	main(void)
 {
 	DDRB |= (1 << 0);
 	DDRD &= ~(1 << 2);
 	PORTD |= (1 << 2);
+	int	prev = PIND & (1 << 2);
 	while (1)
 	{
-		if (!(PIND & (1 << 2)))
-			PORTB |= (1 << 0);
-		else
-			PORTB &= ~(1 << 0);
+		int current = PIND & (1 << 2);
+		if (prev != 0 && current == 0)
+		{
+			PORTB ^= (1 << 0);
+			_delay_ms(50);
+		}
+		prev = current;
 	}
 }
