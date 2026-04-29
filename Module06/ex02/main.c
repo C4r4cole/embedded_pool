04e9f9f58672adb9f3ca07b7b5e27be91332043d
C4r4cole <c4r4cole.chevalerie@gmail.com>
2026-04-29 15:30:37 +0200

    Module06 done

--- Module06/ex02/main.c
+++ Module06/ex02/main.c
@@ -6,7 +6,7 @@
 /*   By: fmoulin <fmoulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/14 13:36:10 by fmoulin           #+#    #+#             */
-/*   Updated: 2026/04/28 18:22:36 by fmoulin          ###   ########.fr       */
+/*   Updated: 2026/04/29 15:29:38 by fmoulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -145,100 +145,114 @@ void print_hex_value(uint8_t c)
 		uart_tx('A' + low - 10);
 }
 
-char	*itoa(int n, char *str, int base)
+void communication_init(void)
 {
-	long	nb;
-	int		i;
-	int		sign;
+	// le premier write sert a dire a quelle adresse on parle
+		// on decale l'adresse de 1 bit vers la gauche
+		// car le dernier bit sert a dire si on write ou on read
+			// 0 = write
+			// 1 = read
+	i2c_write(0x38 << 1); // on ne touche pas au dernier bit donc write
+	i2c_write(0xAC); // cet octet est le Trigger Measurement command (lance un conversion maintenant) (etape 2)
+	i2c_write(0x33); // bit de configuration impose par le constructeur (etape 2)
+	i2c_write(0x00); // bit de configuration impose par le constructeur (etape 2)
+}
 
-	if (base < 2 || base > 16)
-		return (0);
-	nb = n;
-	sign = 0;
-	i = 0;
-	if (nb < 0 && base == 10)
+void read_datas(uint8_t *data)
+{
+	// le premier write sert a dire a quelle adresse on parle
+		// on decale l'adresse de 1 bit vers la gauche
+		// car le dernier bit sert a dire si on write ou on read
+			// 0 = write
+			// 1 = read
+	i2c_write((0x38 << 1) | 1); // on met le dernier bit a 1 donc read
+	for (int i = 0; i < 6; i++)
+		data[i] = i2c_read_ack();	// on recupere les 6 premiers octets avec aknowledgement
+	data[6] = i2c_read_nack(); // on recupere le 7eme octet sans acknowledgment
+}
+
+void raw_calculation(float *humidity, float *temperature)
+{
+	uint8_t		data[7];
+	uint32_t 	humidity_raw;
+
+	uint32_t 	temperature_raw;
+
+	
+	read_datas(data);
+	
+	humidity_raw = ((uint32_t)data[1] << 12) | ((uint32_t)data[2] << 4) | ((uint32_t)data[3] >> 4);
+	*humidity = humidity_raw * 100.00 / 1048576.0;
+	// 1048576.0 est le max sur 20 bits et l'humidite va de 0 a 100%
+	
+	temperature_raw = ((uint32_t)(data[3] & 0x0F) << 16) | ((uint32_t)data[4] << 8) | ((uint32_t)data[5]);
+	*temperature = temperature_raw * 200.00 / 1048576.0 - 50.0;
+	// 1048576.0 est le max sur 20 bits et la temperature sur AHT20 va de -50.C a 150.C
+	// l'amplitude totale est donc de 200.C
+		// c'est pour ca qu'on multiplie par 200
+		// et ensuite on decale de -50 (car amplitude -50.C a 150.C)
+}
+
+void average_calculation(float humidity, float temperature, float *hum_avg, float *temp_avg)
+{
+	static float		temp_hist[3] = {0, 0, 0};
+	static float		hum_hist[3] = {0, 0, 0};
+	static int			idx = 0;
+	static int			count = 0;
+	
+	hum_hist[idx] = humidity;
+	temp_hist[idx] = temperature;
+
+	idx = (idx + 1) % 3;
+	if (count < 3)
+		count++;
+	
+	*hum_avg = 0;
+	*temp_avg = 0;
+	for (int i = 0; i < count; i++)
 	{
-		sign = 1;
-		nb = -nb;
+		*hum_avg += hum_hist[i];
+		*temp_avg += temp_hist[i];
 	}
-	if (nb == 0)
-		str[i++] = '0';
-	while (nb > 0)
-	{
-		if (nb % base < 10)
-			str[i++] = (nb % base) + '0';
-		else
-			str[i++] = (nb % base) - 10 + 'A';
-		nb /= base;
-	}
-	if (sign)
-		str[i++] = '-';
-	str[i] = '\0';
-	i--;
-	sign = 0;
-	while (sign < i)
-	{
-		char tmp = str[sign];
-		str[sign] = str[i];
-		str[i] = tmp;
-		sign++;
-		i--;
-	}
-	return (str);
+	
+	*hum_avg /= count;
+	*temp_avg /= count;
+}
+
+void display_data(float hum_avg, float temp_avg)
+{
+	char		buf[10];
+	
+	uart_printstr("Temperature: ");
+	uart_printstr(dtostrf(temp_avg, 0, 0, buf));
+	uart_printstr(".C, Humidity: ");
+	uart_printstr(dtostrf(hum_avg, 0, 1, buf));
+	uart_tx('%');
+	uart_tx('\r');
+	uart_tx('\n');
 }
 
 int	main(void)
 {
-	uint8_t	data[7];
-	char	buf[20];
-	uint32_t humidity_raw;
-	float humidity;
-	uint32_t temperature_raw;
-	float temperature;
+	float		humidity;
+	float		temperature;
+	float		temp_avg = 0;
+	float		hum_avg = 0;
+	
 	uart_init(16);
 	i2c_init();
 	while (1)
 	{
 		i2c_start();
-		// le premier write sert a dire a quelle adresse on parle
-			// on decale l'adresse de 1 bit vers la gauche
-			// car le dernier bit sert a dire si on write ou on read
-				// 0 = write
-				// 1 = read
-		i2c_write(0x38 << 1); // on ne touche pas au dernier bit donc write
-		i2c_write(0xAC); // cet octet est le Trigger Measurement command (lance un conversion maintenant) (etape 2)
-		i2c_write(0x33); // bit de configuration impose par le constructeur (etape 2)
-		i2c_write(0x00); // bit de configuration impose par le constructeur (etape 2)
+		communication_init();
 		i2c_stop();
-
+		
 		_delay_ms(80); // 80 ms d'attente imposees par le constructeur (etape 3)
-
+		
 		i2c_start();
-		// le premier write sert a dire a quelle adresse on parle
-			// on decale l'adresse de 1 bit vers la gauche
-			// car le dernier bit sert a dire si on write ou on read
-				// 0 = write
-				// 1 = read
-		i2c_write((0x38 << 1) | 1); // on met le dernier bit a 1 donc read
-		for (int i = 0; i < 6; i++)
-		{
-			data[i] = i2c_read_ack();	// on recupere les 6 premiers octets avec aknowledgement
-			// print_hex_value(data[i]);
-			// uart_tx(' ');
-		}
-		data[6] = i2c_read_nack(); // on recupere le 7eme octet sans acknowledgment
-		// print_hex_value(data[6]);
-		humidity_raw = ((uint32_t)data[1] << 12) | ((uint32_t)data[2] << 4) | ((uint32_t)data[3] >> 4);
-		humidity = humidity_raw * 100.00 / 1048576.0;
-		temperature_raw = ((uint32_t)(data[3] & 0x0F) << 16) | ((uint32_t)data[4] << 8) | ((uint32_t)data[5]);
-		temperature = temperature_raw * 200.00 / 1048576.0 - 50.0;
-		uart_printstr("Temperature: ");
-		uart_printstr(dtostrf(temperature, 0, 0, buf));
-		uart_printstr(".C, Humidity: ");
-		uart_printstr(dtostrf(humidity, 0, 1, buf));
-		uart_tx('%');
-		uart_tx('\r');
-		uart_tx('\n');
+		raw_calculation(&humidity, &temperature);
+		average_calculation(humidity, temperature, &hum_avg, &temp_avg);
+		display_data(hum_avg, temp_avg);
 		i2c_stop();
 		
 		_delay_ms(80);
